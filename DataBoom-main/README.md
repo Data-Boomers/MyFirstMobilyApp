5bea8b1ef5e5306811ed19b8daf9b5ffc8afd46d
Data-Boomers <simpson.progamer@gmail.com>
2024-11-13 01:10:11 +0300

    add frontend
    askdh

---
+++ DataBoom-main/README.md
@@ -0,0 +1,2 @@
+data_since.ipynb -> models -> class (там класс для быстрого запуска дефолтных моделек)
+geo_preprocessing.ipynb (там просто коды для геофичей)
---
+++ закл/табличка/utils.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+import random
+import os
+import psutil
+
+
+def set_seed_no_torch(seed=42):
+    np.random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+
+def set_seed_with_torch(seed=42):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+
+def reduce_mem_usage(df):
+    """ iterate through all the columns of a dataframe and modify the data type
+        to reduce memory usage.
+    """
+    start_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+
+    for col in df.columns:
+        if (str(df[col].dtype) not in ['datetime64[ms, Europe/Moscow]', 'datetime64[ms]']) and (col != 'latitude' and col != 'longitude' and col != 'target'):
+            col_type = df[col].dtype.name
+    
+            if col_type not in ['object', 'category', 'datetime64[ns, UTC]']:
+                c_min = df[col].min()
+                c_max = df[col].max()
+                if str(col_type)[:3] == 'int':
+                    if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                        df[col] = df[col].astype(np.int8)
+                    elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                        df[col] = df[col].astype(np.int16)
+                    elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                        df[col] = df[col].astype(np.int32)
+                    elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                        df[col] = df[col].astype(np.int64)
+                else:
+                    if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                        df[col] = df[col].astype(np.float16)
+                    elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                        df[col] = df[col].astype(np.float32)
+                    else:
+                        df[col] = df[col].astype(np.float64)
+
+    end_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+
+    return df
+
+
+def find_cat_features(df):
+    object_cols = df.select_dtypes('object').columns
+    other_cat_cols = []
+    for col in df.columns:
+        if col not in object_cols:
+            if df[col].nunique() / df.shape[0] < 0.0001:
+                other_cat_cols.append(col)
+    return list(object_cols), other_cat_cols
+
+def cpu_usage():
+
+    # Get the memory usage
+    memory_info = psutil.virtual_memory()
+    
+    # Display overall memory usage
+    print(f"Total Memory: {memory_info.total / (1024 ** 3):.2f} GB")
+    print(f"Available Memory: {memory_info.available / (1024 ** 3):.2f} GB")
+    print(f"Used Memory: {memory_info.used / (1024 ** 3):.2f} GB")
+    print(f"Memory Percentage: {memory_info.percent}%")
